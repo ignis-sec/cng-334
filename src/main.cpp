1dca465cfcc766db0aa41f7c535d12325d5458b6
Ata <ahakcil@gmail.com>
2019-04-11 22:56:56 +0300

    Trying out std::thread and mutex

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <pthread.h>
+#include <thread>
+#include <mutex>
+
+#define MAX_THREADS 15
+
+std::mutex mtx;
+
+void test(int id){
+	std::unique_lock<std::mutex> lck (mtx,std::defer_lock);
+	//Lock critical section
+	lck.lock();
+	std::cout << "bar" << id << "\n";
+	//release spinlock
+	lck.unlock();
+}
 
 int main(int argc, char *argv[]) {
 	std::cout << "Hello World!" << std::endl;
+	
+	std::thread threads[MAX_THREADS];
+	int counter = 0;
+	//spawn thread pool
+	for(auto &t : threads){
+		t = std::thread(test, counter++);
+	}
+
+	//syncronize termination
+	for(auto &t : threads){
+		t.join();
+	}
 	return 0;
 }
